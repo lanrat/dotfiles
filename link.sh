f1171c4af00426d60a63639d1a96af4505b7ff4c
Ian Foster <mrlanrat@gmail.com>
2013-06-17 14:42:17 -0700

    moved files to own home dir

--- link.sh
+++ link.sh
@@ -3,28 +3,27 @@
 # link.sh
 
 ########## Variables
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # the dir this script is in
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # the dir this script is in
 old=old
-olddir=$dir/$old             # old dotfiles backup directory
+olddir=$cwd/$old             # old dotfiles backup directory
 me=$(basename $0)
+home_config=home
 ##########
 
 # move any existing dotfiles in homedir to old directory, then create symlinks 
-for file in $(ls $dir);
+for file in $(ls $cwd/$home_config);
 do
-    if [[ ($file != $old) && ($file != $me) && ($file != "config") ]];
+    if [ -e ~/.$file ] && [ ! -L ~/.$file ];
     then
-        if [ -e ~/.$file ] && [ ! -L ~/.$file ];
-        then
-            echo "$file exists moving to $old dir"
-            mv ~/.$file $olddir/$file
-        fi
-        if [ ! -L ~/.$file ];
-        then
-            echo "Creating symlink to $file in home directory."
-            ln -s $dir/$file ~/.$file
-        fi
+        echo "$file exists moving to $old dir"
+        mv ~/.$file $olddir/$file
     fi
+    if [ -L ~/.$file ];
+    then
+        rm ~/.$file
+    fi
+    echo "Creating symlink to $file in home directory."
+    ln -s $cwd/$home_config/$file ~/.$file
 done
 
 #create .config file if it does not exist
@@ -35,18 +34,19 @@ then
 fi
 
 # same as above but with the .config directory
-for file in $(ls $dir/config);
+for file in $(ls $cwd/config);
 do
     if [ -e ~/.config/$file ] && [ ! -L ~/.config/$file ];
     then
         echo "config/$file exists moving to $old/config dir"
         mv ~/.config/$file $olddir/config/$file
     fi
-    if [ ! -L ~/.config/$file ];
+    if [ -L ~/.config/$file ];
     then
-        echo "Creating symlink to config/$file in home directory."
-        ln -s $dir/config/$file ~/.config/$file
+        rm ~/.config/$file
     fi
+    echo "Creating symlink to config/$file in home directory."
+    ln -s $cwd/config/$file ~/.config/$file
 done
 
 
