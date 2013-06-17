330a20608a18dff8d7e58f45ead13e6683632fc7
Ian Foster <mrlanrat@gmail.com>
2013-06-17 13:57:52 -0700

    updated link with config dir

--- link.sh
+++ link.sh
@@ -9,20 +9,45 @@ olddir=$dir/$old             # old dotfiles backup directory
 me=$(basename $0)
 ##########
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to old directory, then create symlinks 
 for file in $(ls $dir);
 do
-    if [[ ($file != $old) && ($file != $me) ]];
+    if [[ ($file != $old) && ($file != $me) && ($file != "config") ]];
     then
-        if [ -e ~/.$file ];
+        if [ -e ~/.$file ] && [ ! -L ~/.$file ];
         then
             echo "$file exists moving to $old dir"
-            mv ~/.$file $olddir
+            mv ~/.$file $olddir/$file
         fi
-
-        echo "Creating symlink to $file in home directory."
-        ln -s $dir/$file ~/.$file
+        if [ ! -L ~/.$file ];
+        then
+            echo "Creating symlink to $file in home directory."
+            ln -s $dir/$file ~/.$file
+        fi
     fi
 done
+
+#create .config file if it does not exist
+if [ ! -e $dir/config ];
+then
+    echo "Creating config dir"
+    mkdir -p ~/.config
+fi
+
+# same as above but with the .config directory
+for file in $(ls $dir/config);
+do
+    if [ -e ~/.$file ] && [ ! -L ~/.$file ];
+    then
+        echo "config/$file exists moving to $old/config dir"
+        mv ~/.$file $olddir/config/$file
+    fi
+    if [ ! -L ~/.$file ];
+    then
+        echo "Creating symlink to config/$file in home directory."
+        ln -s $dir/config/$file ~/.config/$file
+    fi
+done
+
 
 echo "All Done!"
