c6877540ea62cb534a86409fd0ac88eabb45e0cd
Ian Foster <mrlanrat@gmail.com>
2013-11-22 17:56:01 -0800

    moved each program into its own directory, started link script rewrite

--- fetch-submodules.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git submodule init
-#git pull --recurse-submodules
-git submodule update --recursive
--- link.sh
+++ link.sh
@@ -1,53 +1,108 @@
 #!/usr/bin/env bash
-############################
-# link.sh
 
-########## Variables
-cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # the dir this script is in
-old=old
-olddir=$cwd/$old             # old dotfiles backup directory
-me=$(basename $0)
-home_config=home
-##########
+# the dir this script is in
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+config_backup_dir="~/config_backup"
+home="~/"
 
-# move any existing dotfiles in homedir to old directory, then create symlinks 
-for file in $(ls $cwd/$home_config);
-do
-    if [ -e ~/.$file ] && [ ! -L ~/.$file ];
+#helper functions
+function link target src {
+    if [ -e $src ] && [ ! -L $src ];
     then
-        echo "$file exists moving to $old dir"
-        mv ~/.$file $olddir/$file
+        echo "$src exists moving to $config_backup_dir dir"
+        mkdir -p $config_backup_dir
+        mv $src $config_backup_dir/
     fi
-    if [ -L ~/.$file ];
+    if [ -L $src ];
     then
-        rm ~/.$file
+        rm $src
     fi
-    echo "Creating symlink to $file in home directory."
-    ln -s $cwd/$home_config/$file ~/.$file
-done
+    echo "Creating symlink for $src in home directory."
+    ln -s $target $src
+}
 
-#create .config file if it does not exist
-if [ ! -e ~/.config ];
-then
-    echo "Creating config dir"
-    mkdir -p ~/.config
-fi
-
-# same as above but with the .config directory
-for file in $(ls $cwd/config);
-do
-    if [ -e ~/.config/$file ] && [ ! -L ~/.config/$file ];
+function downloadSubmodules dir {
+    if [ -e $dir/submodules ];
     then
-        echo "config/$file exists moving to $old/config dir"
-        mv ~/.config/$file $olddir/config/$file
+        while read l;
+        do
+            read -a array <<< $l
+            git clone ${array[0]} $dir/${array[1]}
+        done < $dir/submodules
     fi
-    if [ -L ~/.config/$file ];
-    then
-        rm ~/.config/$file
-    fi
-    echo "Creating symlink to config/$file in home directory."
-    ln -s $cwd/config/$file ~/.config/$file
-done
+}
+
+#functions for each config
+function conky {
+    echo "Linking conky"
+    link $cwd/conky/conkyrc $home/.conkyrc
+    echo "Done!"
+}
+
+function git {
+    echo "Linking git"
+    for file in $(ls $cwd/git);
+    do
+        link $cwd/git/$file $home/.file
+    done
+    echo "Done!"
+}
+
+function shell {
+    echo "Linking shell"
+    for file in $(ls $cwd/shell);
+    do
+        link $cwd/shell/$file $home/.file
+    done
+    echo "Done!"
+}
+
+function tmux {
+    echo "Linking tmux"
+    link $cwd/tmux/tmux.conf $home/.tmux.conf
+    echo "Done!"
+}
+
+function xscreensaver {
+    echo "Linking xscreensaver"
+    link $cwd/xscreensaver/xscreensaver $home/.xscreensaver
+    echo "Done!"
+}
+
+function vim {
+    echo "Linking vim"
+    downloadSubmodules $cwd/vim
+    link $cwd/vim/vimrc $home/.vimrc
+    link $cwd/vim/vim $home/.vim
+    echo "Done!"
+}
+
+function awesome {
+    echo "Linking awesome"
+    downloadSubmodules $cwd/vim
+    mkdir -p $home/.config
+    link $cwd/awesome/awesome $home/.config/awesome
+    echo "Done!"
+}
+
+function openbox {
+    echo "Linking openbox"
+    link $cwd/openbox/openbox $home/.config/openbox
+    echo "Done!"
+}
+
+function terminator {
+    echo "Linking terminator"
+    link $cwd/terminator/terminator $home/.config/terminator
+    echo "Done!"
+}
+
+function tint2 {
+    echo "Linking tint2"
+    link $cwd/tint2/tint2 $home/.config/tint2
+    echo "Done!"
+}
 
 
-echo "All Done!"
+
+
