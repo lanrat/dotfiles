dd4151eb447d8e0dc62ab764b06ddd570a7e3e33
Jonathan Colby <jcolby@yahoo-inc.com>
2014-02-05 13:08:06 -0800

    Scripts for unused components removed

--- scripts/touchpad.sh
+++
@@ -1,6 +0,0 @@
-## Detect and configure touchpad. See 'man synclient' for more info.
-if egrep -iq 'touchpad' /proc/bus/input/devices; then
-	synclient VertEdgeScroll=0
-	synclient HorizTwoFingerScroll=1
-	synclient PalmDetect=1
-fi
