86f429d4e48f52eccf85b29038373129b846f052
Ian Foster <mrlanrat@gmail.com>
2015-01-26 11:25:19 -0800

    touchpad config should now be automatic

--- scripts/touchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-## Detect and configure touchpad. See 'man synclient' for more info.
-if egrep -iq 'touchpad' /proc/bus/input/devices; then
-	synclient VertEdgeScroll=0
-	synclient HorizTwoFingerScroll=1
-	synclient PalmDetect=1
-	synclient VertScrollDelta=-30
-	synclient HorizScrollDelta=-30
-fi
