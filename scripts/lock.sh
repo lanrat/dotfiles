74836705f728516af47885fa3993d14f2e8da6a2
Ian Foster <mrlanrat@gmail.com>
2015-07-19 20:14:48 -0700

    less anoying color

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -7,7 +7,7 @@ locker()
     #scrot $TMPBG
     #convert $TMPBG -scale 10% -scale 1000% $TMPBG
     #i3lock --ignore-empty-password --image $TMPBG
-    i3lock --ignore-empty-password  --color=6600CC
+    i3lock --ignore-empty-password  --color=000000
     #rm $TMPBG
 }
 
