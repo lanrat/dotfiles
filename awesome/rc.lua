b63ce9ce1ef1b2f123e6edd5c310abef0b15c1c7
Ian Foster <i.foster@salesforce.com>
2016-01-12 12:52:18 -0800

    change default layout on rotated screens

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -20,6 +20,32 @@ local blingbling = require("blingbling")
 vicious = require("vicious")
 -- }}}
 
+-- for testing
+function serializeTable(val, name, skipnewlines, depth)
+    skipnewlines = skipnewlines or false
+    depth = depth or 0
+    local tmp = string.rep(" ", depth)
+    if name then tmp = tmp .. name .. " = " end
+    if type(val) == "table" then
+        tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
+        for k, v in pairs(val) do
+            tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
+        end
+        tmp = tmp .. string.rep(" ", depth) .. "}"
+    elseif type(val) == "number" then
+        tmp = tmp .. tostring(val)
+    elseif type(val) == "string" then
+        tmp = tmp .. string.format("%q", val)
+    elseif type(val) == "boolean" then
+        tmp = tmp .. (val and "true" or "false")
+    else
+        tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
+    end
+    return tmp
+end
+
+
+
 -- {{{ Error handling
 if awesome.startup_errors then
     naughty.notify({ preset = naughty.config.presets.critical,
@@ -134,10 +160,19 @@ local layouts = {
 -- {{{ Tags
 tags = {
    names = { "❶", "❷", "❸", "❹", "❺", "❻", "❼", "❽", "❾" },
-   layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1]},
+   --layout = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1]},
+   layout_landscape = { layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1], layouts[1]},
+   layout_portrait = { layouts[9], layouts[9], layouts[9], layouts[9], layouts[9], layouts[9], layouts[9], layouts[9], layouts[9]},
 }
+
 for s = 1, screen.count() do
-   tags[s] = awful.tag(tags.names, s, tags.layout)
+  if screen[s].geometry.height > screen[s].geometry.width then
+    -- portrait
+    tags[s] = awful.tag(tags.names, s, tags.layout_portrait)
+  else
+    -- landscape 
+    tags[s] = awful.tag(tags.names, s, tags.layout_landscape)
+  end
 end
 -- }}}
 
