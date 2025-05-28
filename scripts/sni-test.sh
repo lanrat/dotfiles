535681ca54fedd8a48f75f214038cfb1f0c894ff
Ian Foster <ian@vorsk.com>
2025-05-28 11:13:13 -0700

    updates

--- scripts/sni-test.sh
+++ scripts/sni-test.sh
@@ -5,9 +5,17 @@ if [ "$#" -lt 1 ]; then
   exit 1
 fi
 
+#TEST_IP="1.1.1.1"
+#TEST_STR="cloudflare"
+
+#TEST_IP="142.250.72.78"
+TEST_IP="$(getent ahostsv4 google.com | cut -f1 -d ' ' | head -1)"
+TEST_STR="google"
+
 for SNI_DOMAIN in "$@"; do
   echo "> Testing: $SNI_DOMAIN"
-  if curl -sk --connect-timeout 5 --resolve $SNI_DOMAIN:443:1.1.1.1 "https://$SNI_DOMAIN" | grep -q cloudflare; then
+  #if curl -sk --connect-timeout 5 --resolve $SNI_DOMAIN:443:1.1.1.1 "https://$SNI_DOMAIN" | grep -q cloudflare; then
+  if curl -sk --connect-timeout 5 --resolve "$SNI_DOMAIN:443:$TEST_IP" "https://$SNI_DOMAIN" | grep -q "$TEST_STR"; then
     echo "> PASS: $SNI_DOMAIN"
   fi
 done
